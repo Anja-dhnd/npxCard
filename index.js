7a28a1a38ef77b6e07dbf0d5bb197e3622ed51ca
svanna <anais.dehand@gmail.com>
2021-09-20 16:58:00 +0200

    updated info

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const data = {
   github: chalk.gray("https://github.com/") + chalk.green("Anja-dhnd"),
   linkedin: chalk.gray("https://linkedin.com/in/") + chalk.blue("anais-dhnd"),
   web: chalk.cyan("#"),
-  npx: chalk.red("npx") + " " + chalk.white("anaisdhnd"),
+  npx: chalk.red("npx") + " " + chalk.white("anja-dhnd"),
 
   labelWork: chalk.white.bold("       Work:"),
   labelBlog: chalk.white.bold("     Blog:"),
